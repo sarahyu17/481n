f895f2112b7e06bbfa5fa14474b278632a4a11c1
Sarah <sarahyu@cs.washington.edu>
2018-04-23 19:35:54 -0700

    sql components

--- data_processing.py
+++ data_processing.py
@@ -1,12 +1,25 @@
-import bz2, json, os, csv
+import bz2, json, os, sqlalchemy
 
 class ProcessData:
-    def __init__(self):
-        self.data = {}
 
     # bz2 file format only, read in data
-    def read_data(self, input, output):
+    def process_table(self, input):
         input = bz2.BZ2File(input)
+
+        engine = connSQL()
+        connection = engine.connect()
+
+        for line in input:
+            obj = json.loads(line)
+            sqlquery = ("INSERT INTO posts_small (author, subreddit, body) "
+                        "VALUES ({:s}, {:s}, {:s})".format(obj["author"], obj["subreddit"], obj["body"]))
+            connection.execute(sqlquery)
+
+        connection.close()
+
+
+
+        '''
         with open(output, 'w') as csvfile:
             fieldnames = ['author', 'subreddit', 'body']
             writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
@@ -17,9 +30,7 @@ class ProcessData:
                 writer.writerow({'author': obj['author'],
                                  'subreddit': obj['subreddit'],
                                  'body': obj['body']})
-
-                '''
-                # optional columns in csv
+        # optional columns in csv
 		'id' : obj['id'],
                 'score': obj['score'],
                 'controversiality': obj['controversiality'],
@@ -37,15 +48,19 @@ class ProcessData:
                 }
                 self.data[id] = entry
                 
-                '''
+             '''
 
+    def connSQL(self):
+        print("Connecting to SQL...")
+        engine = sqlalchemy.create_engine('mysql+pymysql://msap:RK58pycr@cubist.cs.washington.edu/msap_selfPres')
+        return engine
 
 def main():
     #file = input('file: ')
     path = os.path.normpath(os.path.join(os.getcwd(), '..', 'data/RC_2017-01.bz2'))
 
     pd = ProcessData()
-    pd.read_data(path, 'test.csv')
+    pd.process_table(path)
 
 
 
