b9a0a7b3e89b13422c03cb22b7491459abd5581a
Sarah <sarahyu@cs.washington.edu>
2018-04-24 21:38:36 -0700

    also fuck html

--- data_processing.py
+++ data_processing.py
@@ -1,4 +1,4 @@
-import bz2, json, os, sqlalchemy, pymysql, langid
+import bz2, json, os, sqlalchemy, pymysql, langid, re, html
 
 class ProcessData:
 
@@ -12,34 +12,39 @@ class ProcessData:
         input = bz2.BZ2File(input)
 
         engine = self.connSQL()
-        connection = engine.connect()
+        conn = engine.connect()
         print("Connected")
-        i = 0
 
+        # for html specific sanitizing
+        htmltag = re.compile('<.*?>')
+
+        i = 0
         print("Starting SQL queries")
         for line in input:
             print(i)
             i +=1
             
             obj = json.loads(line)
-            # sanitize data for SQL input 
             author = obj["author"]
             subreddit = obj["subreddit"]
             body = obj["body"]
+            # remove HTML encodings
+            body = html.unescape(body)
+            body = re.sub(htmltag, '', body)
             # remove special characters
             body.translate({ord(c): " " for c in "!@#$%^&*()[]{};:,./<>?\|`~-=_+\n"})
-            body.replace("'", "\\'")
-
+            body = body.replace("'", "\\'")
+            
             # language filter
             lang = langid.classify(body)
             if lang[0] != "en":
                 continue
  
             sqlquery = ("INSERT INTO posts_small (author, subreddit, body) VALUES ('{:s}', '{:s}', '{:s}');".format(author, subreddit, body))
-            connection.execute(sqlquery)
+            conn.execute(sqlquery)
 
         print("Done.")
-        connection.close()
+        conn.close()
         print("Closed.")
 
 
